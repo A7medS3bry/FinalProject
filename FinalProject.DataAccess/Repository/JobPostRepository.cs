6d9740284a4140138b24197c12f75354f3fa829b
mndour <mndour@gmail.com>
2024-03-10 11:48:21 +0200

    change FinalProjectC to FinalProject

--- FinalProject.DataAccess/Repository/JobPostRepository.cs
+++ FinalProject.DataAccess/Repository/JobPostRepository.cs
@@ -1,6 +1,7 @@
 ﻿using FinalProject.DataAccess.Data;
 using FinalProject.Domain.IRepository;
 using FinalProject.Domain.Models.JobPostAndContract;
+using FinalProject.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +13,41 @@ namespace FinalProject.DataAccess.Repository
     public class JobPostRepository : Repository<JobPost>, IJobPostRepository
     {
         ApplicationDbContext _context;
+
         public JobPostRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
+
         }
-        
-        public void Update(JobPost entity)
+
+
+        public void Update(int id, JobPostDto jobPostDto)
         {
-            JobPost jobPostDB = _context.JobPosts.FirstOrDefault(j =>  j.Id == entity.Id);
-            if (jobPostDB != null)
-            {
-                jobPostDB.Price = entity.Price;
-                jobPostDB.Description = entity.Description;
-                jobPostDB.Favorites = entity.Favorites;
-                jobPostDB.Status = entity.Status;
-                jobPostDB.DurationTime = entity.DurationTime;
-            }
+            // jobPost always exist
+            JobPost NewJobPost = _context.JobPosts.FirstOrDefault(post => post.Id == id);
+
+            NewJobPost.Title = jobPostDto.Title;
+            NewJobPost.Description = jobPostDto.Description;
+            NewJobPost.Price = jobPostDto.Price;
+            NewJobPost.DurationTime = jobPostDto.DurationTime;
+        }
+
+        public void Create(JobPostDto jobPostDto)
+        {
+
+            JobPost jobPost = new JobPost();
+
+
+            jobPost.Title = jobPostDto.Title;
+            jobPost.Description = jobPostDto.Description;
+            jobPost.Price = jobPostDto.Price;
+            jobPost.DurationTime = jobPostDto.DurationTime;
+            jobPost.Status = "Uncompleted";
+            jobPost.CategoryId = 3;
+            jobPost.UserId = "be258344-4614-4f6c-b431-e1a161b2bd26";
+            _context.JobPosts.Add(jobPost);
+            _context.SaveChanges();
+
         }
     }
 }
